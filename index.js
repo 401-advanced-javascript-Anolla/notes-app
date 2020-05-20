41cd545ef92f185aa089dc2b164f0c23b3d70e8f
Anolla <annehaddad96@yahoo.com>
2020-05-20 23:51:19 +0300

    lab3

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 'use strict';
 
 require('dotenv').config();
-const Input = require('./lib/input');
-
-const Note=require('./lib/notes');
-
-
-let NoteObj=new Input();
-
-let createNote=new Note();
 const mongoose = require('mongoose');
+const Input = require('./lib/input');
+const Note = require('./lib/notes');
+
+let noteObject = new Input();
+
+let note = new Note();
+// note.execute(noteObject);
+// noteObject.valid() ? note.execute(noteObject) :'';
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-createNote.execute(NoteObj);
 
-// NoteObj.valid() ? createNote.execute(NoteObj).then(mongoose.disconnect) : help();
+if ((noteObject.action === 'list')) {
+  noteObject.valid() ? note.list(noteObject).then(mongoose.disconnect) : help();
+}
+else if ((noteObject.action === 'delete')) {
+  noteObject.valid() ? note.delete(noteObject) : help();
+}
+else if ((noteObject.action === 'add' || noteObject.action === 'add')) {
+  noteObject.valid() ? note.add(noteObject).then(mongoose.disconnect) : help();
+}
 
 
-
-// function help() {
-//   console.log(`
-//     APP USAGE: -a <your note goes here> OR --add <your note goes here>
-//     `);
-//   process.exit();
-// }
+function help() {
+  console.log(`
+    APP USAGE: -a <your note goes here> OR --add <your note goes here>
+    `);
+  process.exit();
+}
